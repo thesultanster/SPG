a1d58ab1a26a6540fef4f02110be364fe3ef3d89
Sultan Khan <khansultan024@gmail.com>
2016-01-15 15:01:56 -0800

    test

--- app/src/main/java/spg/com/spg/Home.java
+++ app/src/main/java/spg/com/spg/Home.java
@@ -1,14 +1,21 @@
 package spg.com.spg;
 
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Home extends AppCompatActivity {
 
+    TabLayout tabLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.addTab(tabLayout.newTab().setText("HOME"));
+        tabLayout.addTab(tabLayout.newTab().setText("MY STAYS"));
 
 
     }
